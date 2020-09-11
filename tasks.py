bbd6fad8cd72eaf0231bd118585094a506370384
wrist <stoicheia1986@gmail.com>
2020-09-12 00:22:54 +0900

    Now introduced package management by poetry

---
+++ tasks.py
@@ -0,0 +1,9 @@
+from invoke import task
+
+@task
+def run_window_viewer(c):
+    c.run("poetry run streamlit run streamlit_dsp/window_viewer.py")
+
+@task
+def run_filter_designer(c):
+    c.run("poetry run streamlit run streamlit_dsp/filter_designer.py")
