0a1bb3facc939bb158dd5c8fed656edac287f15a
wrist <stoicheia1986@gmail.com>
2020-10-11 08:34:43 +0900

    Refactor room_designer.py and modify it to enable to handle multiple source

--- streamlit_dsp/room_designer.py
+++ streamlit_dsp/room_designer.py
@@ -18,19 +18,10 @@ import soundfile as sf
 import ast_util
 
 
-def main():
-    """## Room shape"""
-
-    room_type = st.sidebar.selectbox("Choose room type", ["ShoeBox", "from corners"])
-    room_dim = st.sidebar.selectbox("Room dimention", ["2D", "3D"])
-
-    room_fs = st.sidebar.number_input("Room sampling frequency", min_value=1, max_value=192000, value=16000)
-
-    max_order = st.sidebar.number_input("max order", min_value=0, value=1)
-    absorption = st.sidebar.number_input("absorption", min_value=0.0, max_value=1.0)
-
+def st_create_room(room_type, room_dim, room_fs, absorption, max_order):
     room = None
     room_size = None
+
     if room_type == "ShoeBox":
         rx = st.sidebar.slider("x", min_value=0.0, max_value=100.0)
         ry = st.sidebar.slider("y", min_value=0.0, max_value=100.0)
@@ -44,22 +35,26 @@ def main():
     elif room_type == "from corners":
         st.write("Not implemented")
 
-    """## Source"""
-    src_num = st.sidebar.number_input("#source", min_value=0, value=0)
-    src_fs = None
-    wav_ary = []
-    wav_name_ary = []
-    if src_num > 0:
-        src_loc_ary = []
-        for i in range(src_num):
-            wav = st.sidebar.file_uploader(f"source {i}", type="wav", encoding=None)
-            if wav:
-                sig, src_fs = sf.read(wav)
-                if room_fs != src_fs:
-                    st.write("room fs is different from source fs")
-                wav_ary.append(sig)
-                wav_name_ary.append(wav)
+    return (room, room_size)
 
+
+def st_source_setting(room_dim, room_size):
+    wav_ary = []
+    src_loc_ary = []
+
+    src_num = st.sidebar.number_input("#source", min_value=0, value=0)
+
+    if src_num > 0:
+        for i in range(src_num):
+            # wave file upload
+            wav_filelike = st.sidebar.file_uploader(f"source {i}", type="wav", encoding=None)
+            if wav_filelike:
+                sig, src_fs = sf.read(wav_filelike)
+                wav_ary.append((wav_filelike, sig, src_fs))
+            else:
+                wav_ary.append(None)
+
+            # source position
             sx = st.sidebar.slider("x", min_value=0.0, max_value=room_size[0], key=f"src{i}_x")
             sy = st.sidebar.slider("y", min_value=0.0, max_value=room_size[1], key=f"src{i}_y")
 
@@ -69,14 +64,14 @@ def main():
             else:
                 src_loc_ary.append([sx, sy])
 
-        if len(wav_ary) > 0:
-            R = np.array(src_loc_ary).T
-            room.add_source(R, signal=wav_ary[0])
+    return (wav_ary, src_loc_ary)
 
-    """## Microphone"""
+
+def st_mic_setting(room_dim, room_size):
+    mic_loc_ary = []
+
     mic_num = st.sidebar.number_input("#mic", min_value=0, value=0)
     if mic_num > 0:
-        mic_loc_ary = []
         for i in range(mic_num):
             st.sidebar.write(f"mic {i}")
             mx = st.sidebar.slider("x", min_value=0.0, max_value=room_size[0], key=f"mic{i}_x")
@@ -87,11 +82,65 @@ def main():
             else:
                 mic_loc_ary.append([mx, my])
 
+    return mic_loc_ary
+
+
+def main():
+    # ==================================================
+    # room setting
+    # ==================================================
+    st.sidebar.write("## Room shape")
+
+    room_type = st.sidebar.selectbox("Choose room type", ["ShoeBox", "from corners"])
+    room_dim = st.sidebar.selectbox("Room dimention", ["2D", "3D"])
+
+    room_fs = st.sidebar.number_input("Room sampling frequency", min_value=1, max_value=192000, value=16000)
+
+    max_order = st.sidebar.number_input("max order", min_value=0, value=1)
+    absorption = st.sidebar.number_input("absorption", min_value=0.0, max_value=1.0)
+
+    room, room_size = st_create_room(room_type, room_dim, room_fs, absorption, max_order)
+
+    # ==================================================
+    # source setting
+    # ==================================================
+    st.sidebar.write("## Source")
+
+    wav_loc_tup = st_source_setting(room_dim, room_size)
+
+    wav_ary = wav_loc_tup[0]  # list of (filelike, ndarray, fs)
+    src_loc_ary = wav_loc_tup[1]
+
+    # add sources to room
+    src_fs = None
+    for src_loc, wav_tup in zip(src_loc_ary, wav_ary):
+        R = np.array(src_loc).T
+        if wav_tup is not None:
+            src_fs = wav_tup[2]
+            if room_fs != src_fs:
+                st.write("room fs is different from source fs")
+
+            room.add_source(R, signal=wav_tup[1])
+        else:
+            room.add_source(R)
+
+    # ==================================================
+    # microphone setting
+    # ==================================================
+    st.sidebar.write("## Microphone")
+    mic_loc_ary = st_mic_setting(room_dim, room_size)
+    mic_num = len(mic_loc_ary)
+
+    if len(mic_loc_ary) > 0:
         R = np.array(mic_loc_ary).T
         room.add_microphone_array(pra.MicrophoneArray(R, fs=room_fs))
 
-    """# room designer"""
+    # ==================================================
+    # body
+    # ==================================================
+    st.write("# room designer")
     st.write(f"room fs: {room.fs}, source fs: {src_fs}")
+
     fig, ax = room.plot()
     ax.set_xlim([0, room_size[0]])
     ax.set_ylim([0, room_size[1]])
@@ -99,21 +148,36 @@ def main():
         ax.set_zlim([0, room_size[2]])
     st.pyplot(fig)
 
+    # ==================================================
+    # simulation
+    # ==================================================
     ret = st.button("Simulate")
     if ret:
-        room.simulate()
+        exist_all_src = all([wav_tup is not None for wav_tup in wav_ary])
 
-        st.write("source")
-        st.audio(wav_name_ary[0])
+        if exist_all_src:
+            room.simulate()
+            for si, wav_tup in enumerate(wav_ary):
+                if wav_tup is not None:
+                    st.write(f"source {si}")
+                    st.audio(wav_tup[0])
+        else:
+            room.compute_rir()
 
-        for i in range(mic_num):
-            st.write(f"impulse response to mic{i}")
-            st.line_chart(room.rir[i][0])
+        for mi in range(mic_num):
+            for si, wav_tup in enumerate(wav_ary):
+                st.write(f"impulse response from source{si} to mic{mi}")
+                st.line_chart(room.rir[mi][si])
 
-            fp = tempfile.NamedTemporaryFile()
-            sf.write(fp.name, room.mic_array.signals[i], src_fs, format="wav")
-            st.audio(fp.name)
+            if room.mic_array.signals is not None:
+                st.write(f"microphone{mi} observed signal")
+                fp = tempfile.NamedTemporaryFile()
+                sf.write(fp.name, room.mic_array.signals[mi], src_fs, format="wav")
+                st.audio(fp.name)
 
+    # ==================================================
+    # code generation
+    # ==================================================
     ret = st.button("generate code")
     if ret:
         fname = __file__
@@ -125,20 +189,10 @@ def main():
                     "room_fs": room_fs,
                     "max_order": max_order,
                     "absorption": absorption,
-                    "mic_num": mic_num,
-                    "src_num": src_num,
-                    "rx": rx,
-                    "ry": ry,
-                    "rz": rz,
-                    "mic_loc_ary": mic_loc_ary,
-                    "src_loc_ary": src_loc_ary,
-                    # TODO: replace as symbol
-                    "mx": "mic_loc_ary[i][0]",
-                    "my": "mic_loc_ary[i][1]",
-                    "mz": "mic_loc_ary[i][2]",
-                    "sx": "src_loc_ary[i][0]",
-                    "sy": "src_loc_ary[i][1]",
-                    "sz": "src_loc_ary[i][2]",
+                    "wav_loc_tup" : None,
+                    "wav_ary": wav_loc_tup[0],
+                    "src_loc_ary": wav_loc_tup[1],
+                    "mic_loc_ary" : mic_loc_ary,
                  })
 
         st.write("""```python
