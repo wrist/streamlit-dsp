a3ca09abf53cd0e73db564c2cadcaa85eee62d66
wrist <stoicheia1986@gmail.com>
2020-10-10 16:29:15 +0900

    Modify window_viewer to use st.multiselect

--- streamlit_dsp/window_viewer.py
+++ streamlit_dsp/window_viewer.py
@@ -12,80 +12,110 @@ import pandas as pd
 import altair as alt
 
 def main():
-    """
-    # window viewer
-    """
+    st.write("# window viewer")
 
     windows = ["boxcar", "triang", "blackman", "hamming", "hann", "bartlett", "flattop", "parzen", "bohman", "blackmanharris",
             "nuttall", "barthann", "kaiser", "gaussian", "general_gaussian", "slepian", "dpss", "chebwin", "exponential", "tukey"]
 
-    win_name = st.sidebar.selectbox("select window", windows, 4)
+    #win_name = st.sidebar.selectbox("select window", windows, 4)
+    win_names = st.sidebar.multiselect("select window", windows, [windows[4]])
     Nx = st.sidebar.selectbox("Window Length", [2**i for i in range(16)], 8)
     nfft = st.sidebar.selectbox("FFT Length", [2**i for i in range(16)], 10)
     fs = st.sidebar.number_input("Sampling Frequency", min_value=1.0, step=1.0, value=16000.0)
 
     eps = 1.e-16
+    f = (fs / nfft) * np.arange(-nfft/2, nfft/2)
 
     # TODO: need to add UI for other windows require extra parameters
-    win_arg = None
-    if win_name in ["kaiser"]:
-        beta = st.sidebar.number_input("beta")
-        win_arg = (win_name, beta)
-    else:
-        win_arg = win_name
+    win_ary = []
+    W_power_ary = []
 
-    win = sg.get_window(win_arg, Nx)
+    for win_name in win_names:
+        win_arg = None
+        if win_name in ["kaiser"]:
+            beta = st.sidebar.number_input("beta")
+            win_arg = (win_name, beta)
+        else:
+            win_arg = win_name
 
-    W = fft.fft(win, nfft)
-    W_power = 20.0 * np.log10(np.abs(W) + eps)
-    W_power -= W_power[0]
+        win = sg.get_window(win_arg, Nx)
 
-    f = (fs / nfft) * np.arange(-nfft/2, nfft/2)
+        W = fft.fft(win, nfft)
+        W_power = 20.0 * np.log10(np.abs(W) + eps)
+        W_power -= W_power[0]
+        W_power = fft.fftshift(W_power)
+
+        win_ary.append(win)
+        W_power_ary.append(W_power)
 
     # ==================================================
     # using matplotlib
     # ==================================================
-    #fig, axes = plt.subplots(2, 1)
+    # fig, axes = plt.subplots(2, 1)
 
-    #axes[0].plot(win)
-    #axes[0].grid(b=True)
+    # for win in win_ary:
+    #     axes[0].plot(win)
+    # for W_power in W_power_ary:
+    #     axes[1].plot(f, W_power)
 
-    #axes[1].plot(f, fft.fftshift(W_power))
-    #axes[1].grid(b=True)
-    #axes[1].set_ylim([-120.0, 10.0])
+    # axes[0].grid(b=True)
+    # axes[1].grid(b=True)
+    # axes[1].set_ylim([-140.0, 10.0])
 
-    #st.pyplot(fig)
+    # st.pyplot(fig)
 
     # ==================================================
     # using st.line_chart
     # ==================================================
-    #st.line_chart(win)
+    df_win = pd.DataFrame(np.array(win_ary).T,
+                          index=np.arange(Nx),
+                          columns=win_names)
 
-    #df = pd.DataFrame(data=fft.fftshift(W_power), index=f)
-    #st.line_chart(df)
+    df_freq = pd.DataFrame(data=np.array(W_power_ary).T,
+                           index=f,
+                           columns=win_names)
+
+    #st.line_chart(df_win)
+    #st.line_chart(df_freq)
 
     # ==================================================
     # using st.altair_chart
+    # see: https://github.com/streamlit/streamlit/issues/1129
+    #      https://github.com/altair-viz/altair/issues/271
     # ==================================================
-    df = pd.DataFrame({"x": np.arange(Nx), "y": win})
-    c = alt.Chart(df).mark_line().encode(
-            alt.X("x", title="coeff index"),
+    df_win_melt = df_win.reset_index().melt("index", var_name="windows", value_name="y")
+    c = alt.Chart(df_win_melt).mark_line().encode(
+            alt.X("index", title="coeff index"),
             alt.Y("y", title="amplitude"),
-            tooltip=["x", "y"]
+            tooltip=["index", "y"],
+            color='windows:N'
             ).interactive()
     st.altair_chart(c, use_container_width=True)
 
-    df = pd.DataFrame({"x": f, "y": fft.fftshift(W_power)})
-
-    c = alt.Chart(df).mark_line().encode(
-            alt.X("x", title="frequency [Hz]"),
+    df_freq_melt = df_freq.reset_index().melt("index", var_name="windows_power", value_name="y")
+    c = alt.Chart(df_freq_melt).mark_line().encode(
+            alt.X("index", title="frequency [Hz]"),
             alt.Y("y", title="power [dB]", scale=alt.Scale(domain=[-140, 10])),
-            tooltip=["x", "y"]
+            tooltip=["index", "y"],
+            color='windows_power:N'
             ).interactive()
     st.altair_chart(c, use_container_width=True)
 
 
-    st.write("{0}, win.shape: {1}, W.shape: {2}".format(win_name, win.shape, W.shape))
+    # ==================================================
+    # show information
+    # ==================================================
+    st.write("## windows information")
+    st.write("{0}, win.shape: {1}, W.shape: {2}".format(win_names, win.shape, W.shape))
+    st.write("df_win")
+    st.write(df_win)
+    st.write("df_win_melt")
+    st.write(df_win_melt)
+    st.write("df_freq")
+    st.write(df_freq)
+    st.write("df_freq_melt")
+    st.write(df_freq_melt)
+
 
 if __name__ == '__main__':
     main()
